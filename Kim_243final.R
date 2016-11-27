c8e9ec5e5bee66fbdd71e27eb444cb5ab9be475b
Allen Tang <allentang.me@gmail.com>
2016-11-26 17:56:25 -0800

    add plot functions

--- Kim_243final.R
+++ Kim_243final.R
@@ -3,7 +3,21 @@ library("MFAg")
 library("boot")
 library("psych")
 url <- "https://raw.githubusercontent.com/ucb-stat243/stat243-fall-2016/master/problem-sets/final-project/data/wines.csv"
-data <- read.csv(url)
+col_names = c("ID", "1: cat pee", "1: passion fruit", "1: green pepper",
+              "1: mineral", "1: smoky", "1: citrus", "2: cat pee", "2: passion fruit",
+              "2: green pepper", "2: mineral", "2: tropical", "2: leafy",
+              "3: cat pee", "3: passion fruit", "3: green pepper", "3: mineral",
+              "3: grassy", "3: flinty", "4: cat pee", "4: passion fruit",
+              "4: green pepper", "4: mineral", "4: leafy", "5: cat pee",
+              "5: passion fruit", "5: green pepper", "5: mineral", "5: vegetal",
+              "5: hay", "6: cat pee", "6: passion fruit", "6: green pepper", "6: mineral",
+              "6: melon", "7: cat pee", "7: passion fruit", "7: green pepper",
+              "7: mineral", "8: cat pee", "8: passion fruit", "8: green pepper",
+              "8: mineral", "8: grass", "8: smoky", "9: cat pee", "9: passion fruit",
+              "9: green pepper", "9: mineral", "9: peach", "10: cat pee",
+              "10: passion fruit", "10: green pepper", "10: mineral", "titratable acidity",
+              "pH", "alcohol", "residual sugar")
+data <- read.csv(url, col.names = col_names)
 
   # Computing Rv coefficient
 
@@ -228,7 +242,8 @@ mfa <- function(data, sets, supplData, ncomps = NULL, center = TRUE, scale = TRU
               "Compromise contributions across observations" = cntr_obs,
               "Compromise contributions across variables" = cntr_var,
               "Compromise contributions across tables" = cntr_tab,
-              "Labels" = labels)
+              "Labels" = labels,
+              "Column Names" = colnames(data))
   class(res) <- append(class(res), "mfaClass")
   return(res)
 }
--- MFA/R/MFAHelperFunctions.R
+++ MFA/R/MFAHelperFunctions.R
@@ -10,9 +10,13 @@ print.mfa <- function(p, ...)
 }
 
 plot.mfa <- function(p) {
-  #TODO: Plot the plot functions implemented by Allen
+  plot_eigenvalues(p)
+  plot_factor_scores(p)
+  plot_partial_factor_scores(p)
+  for (i in 1:length(obj$"Partial factor scores by assessor")){
+    plot_variable_loadings(p, i)
+  }
 }
-
 
 Eigenvalues <- function(p) {
   #make sure that the parameter p is of class type mfa
@@ -58,6 +62,99 @@ CtrTableToDimension <- function(p) {
 
 
 ##################################################################
+## Plot functions
+##################################################################
+
+plot_eigenvalues <- function(obj){
+  ylim <- c(0, 1.1 * max(obj$Eigenvalues))
+  plot <- barplot(obj$Eigenvalues, main = "Eigenvalues", ylim = ylim)
+  text(x = plot, y = obj$Eigenvalues,
+       label = round(obj$Eigenvalues, digit=2), col = "red", cex = 0.8)
+}
+
+plot_factor_scores <- function(obj){
+  ncomps = ncol(obj$"Compromise factor scores")
+  if (ncomps >= 2) {
+    F_score = obj$"Compromise factor scores"
+    plot(F_score,
+         xlab = "First Principal Component",
+         ylab = "Second Principal Component",
+         main = "Factor Scores",
+         xlim=c(min(F_score[, 1]) - 0.1, max(F_score[, 1]) + 0.1),
+         ylim=c(min(F_score[, 2]) - 0.1, max(F_score[, 2]) + 0.1)
+    )
+    abline(h = 0, v = 0, lty=2)
+    text(F_score, as.vector(obj$"Labels"), pos=3, col = "red")
+  }
+}
+
+plot_partial_factor_scores <- function(obj, accessor_number=0, wine_number=0){
+  plot_factor_scores(obj)
+  partial_scores = obj$"Partial factor scores by assessor"
+  factor_scores = obj$"Compromise factor scores"
+  if (accessor_number != 0) {
+    i = accessor_number
+    for (j in 1:nrow(factor_scores)) {
+      points(partial_scores[[i]][j, 1], partial_scores[[i]][j, 2],
+             pch = 24, cex = 0.2)
+      segments(partial_scores[[i]][j, 1], partial_scores[[i]][j, 2],
+               factor_scores[j, 1], factor_scores[j, 2],
+               lty = 3, lwd = 0.8, col = 'blue')
+    }
+    title(sub=paste("with Partial Factor Scores of accessor ", accessor_number))
+  }
+  if (wine_number != 0) {
+    j = wine_number
+    for (i in 1:length(partial_scores)) {
+      points(partial_scores[[i]][j, 1], partial_scores[[i]][j, 2],
+             pch = 24, cex = 0.2)
+      segments(partial_scores[[i]][j, 1], partial_scores[[i]][j, 2],
+               factor_scores[j, 1], factor_scores[j, 2],
+               lty = 3, lwd = 0.8, col = 'blue')
+    }
+    title(sub=paste("with Partial Factor Scores of item ", wine_number))
+  }
+  if (wine_number == 0 && accessor_number == 0) {
+    for (j in 1:nrow(factor_scores)) {
+      for (i in 1:length(partial_scores)) {
+        points(partial_scores[[i]][j, 1], partial_scores[[i]][j, 2],
+               pch = 24, cex = 0.2)
+        segments(partial_scores[[i]][j, 1], partial_scores[[i]][j, 2],
+                 factor_scores[j, 1], factor_scores[j, 2],
+                 lty = 3, lwd = 0.8, col = 'blue')
+      }
+    }
+    title(sub="with Partial Factor Scores")
+  }
+}
+
+plot_variable_loadings <- function(obj, accessor_number=0){
+  partial_scores = obj$"Partial factor scores by assessor"
+  loadings = obj$"Matrix of loadings"
+  if (accessor_number == 0) {
+    accessor_number = 1
+  }
+  ncomps = ncol(obj$"Compromise factor scores")
+  if (ncomps >= 2) {
+    plot(partial_scores[[accessor_number]],
+         xlab = "First Principal Component",
+         ylab = "Second Principal Component",
+         main = "Partial Factor Scores with Variable Loadings",
+         xlim=c(min(partial_scores[[accessor_number]][, 1]) - 0.1,
+                max(partial_scores[[accessor_number]][, 1]) + 0.1),
+         ylim=c(min(partial_scores[[accessor_number]][, 2]) - 0.1,
+                max(partial_scores[[accessor_number]][, 2]) + 0.1)
+    )
+    abline(h = 0, v = 0, lty=2)
+    text(partial_scores[[accessor_number]], as.vector(obj$"Labels"), pos=3, col = "red")
+    points(loadings[sets[[accessor_number]]-1, ], pch = 24)
+    text(loadings[sets[[accessor_number]]-1, ], as.vector((obj$"Column Names")[sets[[accessor_number]]-1]),
+         pos=3, col = "blue", cex=0.5)
+    title(sub=paste("with Partial Factor Scores of accessor ", accessor_number))
+  }
+}
+
+##################################################################
 ## Helper functions to check that the arguments passed are correct
 ##################################################################
 
@@ -73,7 +170,7 @@ checkScaleOrCenter <- function(param, maxCount) {
       return(TRUE)
     }
   }
-  else 
+  else
   {
     stop("Scale or center parameter must either be a numeric vector or a boolean value (TRUE or FALSE)")
   }
@@ -96,7 +193,7 @@ checkData <- function(data)
 
     if(!(is.data.frame(data) || is.matrix(data)))
     {
-      stop("Data parameter must be matrix or data frame") 
+      stop("Data parameter must be matrix or data frame")
     }
     else
     {
@@ -108,7 +205,7 @@ checkData <- function(data)
 checkSets <- function(sets) {
     if(!is.list(sets))
     {
-      stop("Sets input must be a list") 
+      stop("Sets input must be a list")
     }
     else
     {
