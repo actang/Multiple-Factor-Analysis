584c618939019bf0defa09d9bc2246928783bc3a
Ankush Desai <ankushdesai@gmail.com>
2016-12-02 21:11:40 -0800

    updated the vignette

--- MFA/inst/doc/mfa-vignette.R
+++ MFA/inst/doc/mfa-vignette.R
@@ -2,9 +2,8 @@
 knitr::opts_chunk$set(collapse = T, comment = "#>")
 library(mfa)
 
-## ------------------------------------------------------------------------
-mymfa <- function(data, sets, supplData, ncomps = NULL, center = TRUE, scale = TRUE)
-mymfa
+## ---- eval=FALSE---------------------------------------------------------
+#  mfa_out <- function(data, sets, supplData, ncomps = NULL, center = TRUE, scale = TRUE)
 
 ## ---- eval = FALSE-------------------------------------------------------
 #  ncomps_2 <- mfa(..., ncomps = 2)
@@ -15,29 +14,32 @@ mymfa
 #  not_normalized <- mfa(..., scale = FALSE)
 
 ## ---- eval = FALSE-------------------------------------------------------
-#  Eigenvalues(mymfa)
+#  print(mfa_out)
+
+## ---- eval = FALSE-------------------------------------------------------
+#  Eigenvalues(mfa_out)
 
 ## ---- eval=FALSE---------------------------------------------------------
-#  CtrObserToDimension(mymfa)
-#  CtrVarToDimension(mymfa)
-#  CtrTableToDimension(mymfa)
+#  CtrObserToDimension(mfa_out)
+#  CtrVarToDimension(mfa_out)
+#  CtrTableToDimension(mfa_out)
 
 ## ---- eval = FALSE-------------------------------------------------------
 #  # RV
 #  rv_coef <- RV(table1, table2)
-#  rv_table <- RV_table(dataset, sets = list(1:3, 4:5, 6:10))
+#  rv_table <- Rv_table(dataset, sets = list(1:3, 4:5, 6:10))
 #  
 #  # Lg
 #  lg_coef <- Lg(table1, table2)
 #  lg_table <- Lg_table(dataset, sets = list(1:3, 4:5, 6:10))
 
 ## ---- eval = FALSE-------------------------------------------------------
-#  boot_ratio <- boot_ratio(XX)
+#  boot_ratio <- mfa_out$BootstrapRatio
 
 ## ---- eval = FALSE, fig.show='hold'--------------------------------------
-#  plot_eigenvalues(mymfa)
-#  plot_factor_scores(mymfa)
-#  plot_partial_factor_scores(mymfa)
-#  plot_variable_loadings(mymfa)
-#  plot_boot_ratio(mymfa)
+#  plot_eigenvalues(mfa_out)
+#  plot_factor_scores(mfa_out)
+#  plot_partial_factor_scores(mfa_out)
+#  plot_variable_loadings(mfa_out)
+#  plot_boot_ratio(mfa_out, 2) # number of components = 2
 
